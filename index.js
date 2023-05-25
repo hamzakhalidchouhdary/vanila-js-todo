371f4ba6e8456a0b496314a98ddf0d8784b91911
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-05-25 18:58:46 +0500

    added bootstrap styling

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ function createTodoElement(text) {
   const todoItemWrapper = document.createElement('li');
   const todoRemoveBtn = document.createElement('button');
   todoRemoveBtn.innerText = 'Delete';
-  todoItemWrapper.innerHTML = text;
+  todoRemoveBtn.classList.add('btn','btn-danger', 'btn-sm', 'col');
+  todoItemWrapper.classList.add('row')
+  todoItemWrapper.innerHTML = `<p class='col'>${text}</p>`;
   todoItemWrapper.appendChild(todoRemoveBtn);
   return todoItemWrapper
 }
