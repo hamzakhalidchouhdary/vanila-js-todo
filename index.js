f6961b644cd04af9c385c092c2092c24d771af9c
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-05-29 18:39:12 +0500

    added remove todo item method

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const todoContainer = document.querySelector('#todo-container');
 
+function removeListItem({target: item}) {
+  item.parentNode.remove();  
+}
+
 function createTodoElement(text) {
   if (!text) return;
   const todoItemWrapper = document.createElement('li');
   const todoRemoveBtn = document.createElement('button');
   const todoTextEle = document.createElement('p');
   todoRemoveBtn.innerText = 'X';
+  todoRemoveBtn.addEventListener('click', removeListItem);
   todoRemoveBtn.classList.add('btn','btn-outline-danger', 'btn-md', 'col-1');
   todoItemWrapper.classList.add('row', 'py-3', 'border-top', 'border-bottom', 'border-light-subtle');
   todoTextEle.classList.add('col-11', 'm-0');
